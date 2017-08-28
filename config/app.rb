e96e5332412f267406f4187276fb0dc46aafee3d
keoncummings <keon.cummings@gmail.com>
2017-08-28 16:19:33 -0500

    adding sinatra and react

---
+++ config/app.rb
@@ -0,0 +1,15 @@
+# gems and classes...
+require 'bundler'
+Bundler.require
+
+class SinatraReact < Sinatra::Base
+    
+    # configure sinatra app
+    set :public_folder, 'public'
+    
+    # Public folder 
+    get '/' do
+      File.read(File.join('public', 'index.html'))
+    end
+
+end
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,11 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
 const path = require('path');
 
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
+  context: path.join(__dirname, "/public"),
+  devtool: debug ? "inline-sourcemap" : false,
+  entry: './public/src/js/client.js',
      module: {
         rules: [{
             test: /\.scss$/,
@@ -18,6 +16,26 @@ module.exports = {
             }, {
                 loader: "sass-loader" // compiles Sass to CSS 
             }]
-        }]
-    }
+        },
+
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+          plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
+        }
+      }
+    ]
+  },
+   output: {
+    path: __dirname + "/public",
+    filename: "client.min.js"
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
 };
